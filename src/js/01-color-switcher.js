f4eb340e0ec870c9fa3fad3022504102d1188e66
Shundrik <102665832+Shundrik@users.noreply.github.com>
2022-09-11 09:49:43 +0300

    hw-9
    1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+};
+const btnStart = refs.start.addEventListener('click', startChangeColor);
+const btnStop = refs.stop.addEventListener('click', stopChangeColor);
 
+let setID = 0;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+function createColor() {
+  return (document.body.style.backgroundColor = getRandomHexColor());
+}
+
+function startChangeColor() {
+  setID = setInterval(createColor, 1000);
+  // refs.start.removeEventListener("click",()=>{
+  //   console.log("remove");
+  // })
+  refs.start.disabled = true;
+
+}
+
+function stopChangeColor() {
+  clearTimeout(setID);
+  refs.start.disabled = false;
+}
