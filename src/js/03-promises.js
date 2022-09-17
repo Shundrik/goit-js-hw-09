ab0c58ad7c40bd6d78a55e419517f6919befde83
Shundrik <102665832+Shundrik@users.noreply.github.com>
2022-09-17 10:54:23 +0300

    hw-9

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -7,53 +7,62 @@ const refs = {
 
 refs.form.addEventListener('input', valueFromForm);
 refs.btn.addEventListener('submit', createPromise);
-let dataForm = {};
-// const delay = document.querySelector(`[name="delay"] `);
-// const step = refs.form.elements.step.value;
-// const amount = refs.form.elements.amount.value;
+
+let firstDelay = 0;
+let stepDelay = 0;
+let amount = 0;
+
+
+// let dataForm = {};
 
 function valueFromForm(e) {
-dataForm[e.target.name] = e.target.value;
+  
+  // dataForm[e.target.name] = e.target.value;
+// console.log("change");
+  firstDelay = +refs.form.elements.delay.value;
+  stepDelay = +refs.form.elements.step.value;
+  amount = +refs.form.elements.amount.value;
 
-  const delay = refs.form.elements.delay.value;
-  const step = refs.form.elements.step.value;
-  const amount = refs.form.elements.amount.value;
-  console.log(delay);
-  console.log(step);
-  console.log(amount);
- }
-
-
+  console.log(firstDelay);
+}
+console.log(firstDelay);
+console.log(stepDelay);
+console.log(amount);
 
 function createPromise(position, delay) {
-  e.preventDefault();
-   console.log(`start ${position} and ${delay}`);
-  const shouldResolve = Math.random() > 0.3;
-  new Promise((resolve, reject) => {
-    
-  })
 
-  setTimeout(()=>{
+// let firstDelay = 1000;
+// let step = 500;
+// let amount = 5;
 
-    if (shouldResolve) {
-   Resolve({ dataForm });
-    
-   
-  } else {
-    // Reject
-  }
-  }, delay)
+// function createPromise(position, delay) {
+for (let i = 1; i <= amount; i += 1) {
+ let  delay = firstDelay;
+  console.log(delay);
+  let position = 0;
+  const p = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      position += i;
+      // console.log(delay);
+      console.log(`start ${position} and ${delay}`);
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, firstDelay);
+    firstDelay += step;
+  });
 
-  
+  p.then(({ position, delay }) => {
+    console.log(`start ${position} and ${delay}`);
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  }).catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+}
 }
 
-createPromise(2, 1500)
-.then(({ position, delay }) => {
-  console.log(`start ${position} and ${delay}`);
-  console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
-})
-.catch(({ position, delay }) => {
-  console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
-});
