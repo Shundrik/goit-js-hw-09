b24e9c36f3b08584d3265ee672adaa5e0507cdbf
Shundrik <102665832+Shundrik@users.noreply.github.com>
2022-09-25 00:17:38 +0300

    hw-9
    исправил - Перший проміс має виконуватися через проміжок часу, зазначений в інпуті First delay

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -12,25 +12,30 @@ refs.form.addEventListener('submit', e => {
   let step = +e.currentTarget.step.value;
   let amount = +e.currentTarget.amount.value;
 
-
   for (let i = 1; i <= amount; i += 1) {
-  delay += step;
-    // let position = i
     createPromise(i, delay)
-    .then(({ position, delay }) => {
-              // console.log(`start ${position} and ${delay}`);
-              // console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-              Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-            }).catch(({ position, delay }) => {
-              // console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-              Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-            });
+      .then(({ position, delay }) => {
+        // console.log(`start ${position} and ${delay}`);
+        // console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        // console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delay += step;
   }
 });
 function createPromise(position, delay) {
-  console.log("ddddd");
+  console.log(delay);
+
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
+    console.log(delay);
     setTimeout(() => {
       if (shouldResolve) {
         resolve({ position, delay });
